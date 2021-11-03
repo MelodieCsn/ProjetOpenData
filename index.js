86aef2742b16670e4821704a30c87d1ce8624986
moulikatou <moulikahip@gmail.com>
2021-11-03 16:29:05 +0100

    premier jet index.js

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-"use strict";
+"use strict"; // pour faire du java script propre ( tout déclarer)
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 3000;
+
+const FORMATS = ["application/json", "application/xml", "application/rdf+xml"];
+
+app.use(function(req, res, next) {
+  let format = req.get('Accept');
+
+  if (format === "*/*" || format === undefined || format === "") {
+    format = "application/json";
+  }
+  
+  if (!FORMATS.includes(format)) {
+    let err = new Error("Mauvais format demandé dans le header \"Accept\".\nLes formats acceptés sont:\n- application/json (valeur par défaut)\n- application/xml\n- application/rdf+xml")
+    err.statusCode = 400
+    next(err)
+  }
+  next();
+});
+
 
 app.get('/', function (req, res) {
-  res.send('Bienvenue sur petits emprunts bientôt en react !');
+  res.write('\nBienvenue sur petits emprunts bientôt en react !');
+  res.end();
+});
+
+app.get('/regions', function (req, res) {
+  res.write(req.header('user-agent'));
+  res.end();
+});
+
+app.get('/regions/:regionId', function (req, res) {
+  res.write(req.params.regionId);
+  res.write(req.query.city);
+  res.end();
 });
 
 app.listen(PORT, function () {
-  console.log('Petits emprunts lancé sur le port :' + PORT);
+  console.log('Serveur accessible sur le port ' + PORT);
 });
+
+
+app.use(function (err, req, res, next) {
+  res.status(err.statusCode).send(err.message);
+})
