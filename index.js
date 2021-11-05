12973e9a9fd073ec1df371c236168d572adc1352
MelodieCsn <melodymusyck@gmail.com>
2021-11-05 10:28:32 +0100

    push final

--- index.js
+++ index.js
@@ -79,31 +79,83 @@ function doApi (regionId, date, format, res) {
                     const parsed2 = JSON.parse(body2);
                     const recordsTemp = parsed2.records;
 
-
+                    resultConso.Publisher = "Open Data Réseaux Energies";
                     resultConso.region = regionId;
                     resultConso.periode = date;
 
                     recordsConso.forEach(function(recordsConso) {
                         recordsTemp.forEach(function(recordsTemp) {
                             if (recordsTemp.fields.date === recordsConso.fields.date) {
-                                conso.push({
-                                    date_mesure : recordsConso.fields.date,
-                                    conso_rte : recordsConso.fields.consommation_brute_electricite_rte,
-                                    region : recordsConso.fields.region,
-                                    temperature : recordsTemp.fields.tmoy
-                                })
-                            }
+                              conso.push( {Date_mesure : recordsConso.fields.date,
+                                          Region :{
+                                              Region_name : recordsConso.fields.region,
+                                              Region_code : recordsConso.fields.code_insee_region},
+                                              Temperature : {
+                                                    Temperature_moyenne : recordsTemp.fields.tmoy,
+                                                    Temperature_minimale : recordsTemp.fields.tmin,
+                                                    Temperature_maximale : recordsTemp.fields.tmax
+                                                  },
+                                                  Consommation_energetique : {
+                                                    Conso_elec : recordsConso.fields.consommation_brute_electricite_rte,
+                                                    Conso_gaz : recordsConso.fields.consommation_brute_gaz_totale,
+                                                  }
+                                          })
+                                }
                         })
                     })
 
-                    resultConso.features = conso;
+                    resultConso.Mesure = conso;
+
+                    var xml_rdf =  `<?xml version="1.0"?>\n`
+                    xml_rdf += `<rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns"
+                            xmlns:sql="http://ns.inria.fr/ast/%22%3E"
+                            xmlns:oml = "http://def.seegrid.csiro.au/ontology/om/om-lite"
+                            xmlns:lsweb = "http://ontology.cybershare.utep.edu/ELSEWeb/elseweb-data.owl"
+                            xmlns:dc = "http://purl.org/dc/elements/1.1/"
+                            xmlns:igeo = "http://rdf.insee.fr/def/geo"
+                            xmlns:place = "http://purl.org/ontology/places"
+                            xmlns:s4bldg  = "https://saref.etsi.org/saref4bldg/"
+                            xmlns:s4wear = "https://saref.etsi.org/saref4wear/"
+                            xmlns:ddesc= "https://w3id.org/arco/ontology/denotative-description/"
+                            xmlns:shw = "http://paul.staroch.name.thesis/SmartHomeWeather.owl">\n`
+
+                    recordsConso.forEach(function(recordsConso){
+                      recordsTemp.forEach(function(recordsTemp){
+                        if (recordsTemp.fields.date == recordsConso.fields.date){
+                          xml_rdf += `\t<oml:MeasureObject>\n`
+                          xml_rdf += `\t\t<dc:Publisher rdf:property = "http://purl.org/dc/elements/1.1/publisher">\n`
+                          xml_rdf += `\t\t\t<dc:Agent rdf:about="https://opendata.reseaux-energies.fr/api/records/1.0/search/"/>\n`
+                          xml_rdf += `\t\t</dc:Publisher>\n`
+                          xml_rdf += `\t\t<lsweb:hasDate>` + recordsConso.fields.date + `</lsweb:hasDate>\n`
+                          xml_rdf += `\t\t<s4wear:isLocated>\n`
+                          xml_rdf += `\t\t\t<dc:Location>\n`
+                          xml_rdf += `\t\t\t\t<igeo:codeRegion rdf:datatype = "http://www.w3.org/2001/XMLSchema#int">` + recordsConso.fields.code_insee_region + `</igeo:codeRegion>\n`
+                          xml_rdf += `\t\t\t\t<place:Region rdf:datatype = "http://www.w3.org/2001/XMLSchema#string">` + recordsConso.fields.region + `</place:Region>\n`
+                          xml_rdf += `\t\t\t</dc:Location>\n`
+                          xml_rdf += `\t\t</s4wear:isLocated>\n`
+                          xml_rdf += `\t\t<ddesc:hasMeasurement>\n`
+                          xml_rdf += `\t\t\t<s4bldg:nominalPowerConsumption rdf:datatype = "http://www.w3.org/2001/XMLSchema#int">` + recordsConso.fields.consommation_brute_electricite_rte + `</s4bldg:nominalPowerConsumption>\n`
+                          xml_rdf += `\t\t</ddesc:hasMeasurement>\n`
+                          xml_rdf += `\t\t<shw:hasTemperatureValue>\n`
+                          xml_rdf += `\t\t\t<shw:Temperature>\n`
+                          xml_rdf += `\t\t\t\t<sql:avg rdf:datatype = "http://www.w3.org/2001/XMLSchema#float" >` + recordsTemp.fields.tmoy + `</sql:avg>\n`
+                          xml_rdf += `\t\t\t\t<sql:min rdf:datatype = "http://www.w3.org/2001/XMLSchema#float" >` + recordsTemp.fields.tmin + `</sql:min>\n`
+                          xml_rdf += `\t\t\t\t<sql:max rdf:datatype = "http://www.w3.org/2001/XMLSchema#float" >` + recordsTemp.fields.tmax + `</sql:max>\n`
+                          xml_rdf += `\t\t\t</shw:Temperature>\n`
+                          xml_rdf += `\t\t</shw:hasTemperatureValue>\n`
+                          xml_rdf += `\t</oml:MeasureObject>\n`
+
+                        }
+                      })
+                    })
+                      xml_rdf += `</rdf:RDF>`
 
                     if (format === "application/json") {
                         res.json(resultConso);
                     } else if (format === "application/xml") {
                         res.send(js2xmlparser.parse("regions", resultConso));
                     } else if (format === "application/rdf+xml") {
-                        res.send("");
+                        res.send(xml_rdf);
                     }
                 })
         })
